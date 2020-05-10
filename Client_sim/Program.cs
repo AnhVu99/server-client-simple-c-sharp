74e00bc5044596d6faa5e000741219793676974d
Anh Vu <thav06101999@gmail.com>
2020-05-10 10:58:15 +0700

    init repository

---
+++ Client_sim/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            // 1. connect
+            client.Connect("192.168.1.136", 5000);
+            Stream stream = client.GetStream();
+
+            Console.WriteLine("Connected to Server_sim");
+            while (true)
+            {
+                Console.Write("'Exit to exit' ");
+                Console.Write("Enter your name: ");
+
+                string str = Console.ReadLine();
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+
+                // 2. send
+                writer.WriteLine(str);
+
+                // 3. receive
+                str = reader.ReadLine();
+                Console.WriteLine(str);
+                if (str.ToUpper() == "BYE")// nhận bye sẽ ngắt vòng lập
+                    break;
+            }
+            // 4. close
+            stream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ Server_Sim/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server_Sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress address = IPAddress.Parse("192.168.1.136");
+
+            TcpListener listener = new TcpListener(address, 5000);
+
+            listener.Start();
+
+            Console.WriteLine("Server started on " + listener.LocalEndpoint);
+            Console.WriteLine("Waiting for a connection...");
+
+            Socket socket = listener.AcceptSocket();
+            Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+
+            var stream = new NetworkStream(socket);
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;//tự động gửi dữ liệu mà không cần đợi bộ đệm đầy hoặc bạn phải gọi thủ công phương thức Flush()
+            while (true)
+            {
+                // 2. receive
+                string str = reader.ReadLine();
+                if (str.ToUpper() == "EXIT")// khi nhận exit sẽ trả về bye
+                {
+                    writer.WriteLine("bye");
+                    break;
+                }
+                // 3. send
+                writer.WriteLine("Hello " + str);
+            }
+        }
+    }
+}
